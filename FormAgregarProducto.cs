13ec0dcd82b3c004edc9ad8e0f7a5f3e32e103c2
Harold <harold.berru24@gmail.com>
2024-07-21 15:04:28 -0500

    SE AGREGO REGISTRO DE PRODCUTOS

---
+++ FormAgregarProducto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiventas_Krisstelle
+{
+    public partial class FormAgregarProducto : Form
+    {
+        public FormAgregarProducto()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtStockMinimo.Text = "Stock mínimo";
+            txtStockMinimo.ForeColor = SystemColors.GrayText;
+        }
+
+        private void txtStockMinimo_Enter(object sender, EventArgs e)
+        {
+            if (txtStockMinimo.Text == "Stock mínimo")
+            {
+                txtStockMinimo.Text = "";
+                txtStockMinimo.ForeColor = SystemColors.WindowText;
+            }
+        }
+
+        private void txtStockMinimo_Leave(object sender, EventArgs e)
+        {
+            if (txtStockMinimo.Text == "")
+            {
+                txtStockMinimo.Text = "Stock mínimo";
+                txtStockMinimo.ForeColor = SystemColors.GrayText;
+            }
+        }
+    }
+}
