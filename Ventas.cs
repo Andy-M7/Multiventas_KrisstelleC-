391317dcfab39127926e575b08c6357c22ceb810
Harold <harold.berru24@gmail.com>
2024-07-21 22:21:27 -0500

    VENTAS

--- Ventas.cs
+++ Ventas.cs
@@ -16,5 +16,10 @@ namespace Multiventas_Krisstelle
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
